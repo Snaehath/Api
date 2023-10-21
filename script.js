942bb7b11ef09a9e7d2cf5878b431c7db1613d20
Snaehath <2020ad0556@svce.ac.in>
2023-10-21 20:13:03 +0530

    Added web features

--- script.js
+++ script.js
@@ -1,37 +1,59 @@
+const xml = document.querySelector('.b1')
+const fetchWay = document.querySelector('.b2')
+const axiosWay = document.querySelector('.b3')
+let ouput = document.querySelector('.result')
+
 // XMLHttpRequest
 
-// const req = new XMLHttpRequest()
+const req = new XMLHttpRequest()
 
-// req.onload = function(){
-//     console.log("Succes")
-//     const data = JSON.parse(this.responseText)
-//     console.log(data)
-// }
+req.onload = function(){
+    console.log("Succes(XML)")
+    const data = JSON.parse(this.responseText)
+    console.log(data)
+    ouput.textContent = "XMLRequest is Sucess"
+}
 
-// req.onerror = function() {
-//     console.log("Error")
-//     console.log(this)
-// }
+req.onerror = function() {
+    console.log("Error(XML)")
+    console.log(this)
+}
 
-// req.open('GET',"https://official-joke-api.appspot.com/random_joke")
-// req.send();
+req.open('GET',"https://official-joke-api.appspot.com/random_joke")
+
+xml.addEventListener('click',() =>{
+    req.send();
+})
 
 // fetch
 
-// fetch("https://official-joke-api.appspot.com/random_joke")
-// .then(res => {
-//     res.json().then(data => console.log("Resolved and Parsed",data))
-// })
-// .catch(e => {
-//     console.log("Error",e)
-// })
+function secondWay(api) {
+    fetch(api)
+    .then(res => {
+        res.json().then(data => console.log("Resolved and Parsed(fetch)",data))
+        ouput.textContent = "Fetch request is Success"
+    })
+    .catch(e => {
+        console.log("Error(fetch)",e)
+    })
+}
+
+fetchWay.addEventListener('click',() => {
+    secondWay("https://official-joke-api.appspot.com/random_joke")
+})
+
 
 //axios
 
-axios.get("https://official-joke-api.appspot.com/random_joke")
-.then(res => {
-    console.log("Resolved",res.data)
-})
-.catch(e => {
-        console.log("Error",e)
+function thirdWay(api) {
+
+    axios.get(api)
+    .then(res => {
+        console.log("Resolved(Axios)",res.data)
+        ouput.textContent = "Axios request is Success"
     })
+    .catch(e => {
+        console.log("Error(Axios)",e)
+    })
+}
+axiosWay.addEventListener('click',() => thirdWay("https://official-joke-api.appspot.com/random_joke"))
